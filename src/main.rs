90bf9976f890b1089d21f6fc8725141314e530ee
Artyom Vasich <vasich.dev@gmail.com>
2025-06-02 21:11:19 +0400

    more robust /sys/ traversal

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,67 @@
-fn main() -> Result<(), std::io::Error> {
-    let inhibited_path = std::fs::read_dir("/sys/class/input")?.flatten().find_map(|entry| {
-        let name_path = entry.path().join("device/name");
-        std::fs::read_to_string(&name_path).ok().filter(|line| line.to_lowercase().contains("touchpad")).map(|_| entry.path().join("device/inhibited"))
-    });
+use std::{
+    collections::HashSet,
+    path::{Path, PathBuf},
+};
 
-    if let Some(inhibited_path) = inhibited_path {
-        let contents = std::fs::read_to_string(&inhibited_path)?.trim().to_string();
-        let new_value = match contents.as_str() {
-            "0" => "1",
-            "1" => "0",
-            _ => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Invalid content")),
-        };
-        std::fs::write(&inhibited_path, new_value)?;
+
+fn main() -> Result<(), std::io::Error> {
+    let device_name = "Touchpad";
+    if let Some(path) = find_in_class_input(device_name)
+        .or_else(|| find_device(Path::new("/sys/devices"), device_name, &mut HashSet::new()))
+    {
+        toggle_inhibited(&path)?;
     }
     Ok(())
+}
+
+
+/// looks for corresponding 'inhibited' file in /sys/class/input/{device} and /sys/class/input/{device}/device
+fn find_in_class_input(device_name: &str) -> Option<PathBuf> {
+    std::fs::read_dir("/sys/class/input")
+        .ok()?
+        .flatten()
+        .filter(|entry| entry.file_name().to_string_lossy().starts_with("input"))
+        .map(|entry| entry.path())
+        .find_map(|path| check_directory(&path, device_name))
+}
+
+
+/// the directory should contain a 'name' file with the device name and an 'inhibited' file we need
+fn check_directory(path: &Path, device_name: &str) -> Option<PathBuf> {
+    if let Ok(name_line) = std::fs::read_to_string(path.join("name"))
+        && name_line.contains(device_name)
+        && let inhibited_path = path.join("inhibited")
+        && matches!(std::fs::exists(&inhibited_path), Ok(true))
+    {
+        Some(inhibited_path)
+    } else {
+        None
+    }
+}
+
+
+/// we haven't found the device under /sys/class/input and are defaulting to full /sys/devices traversal.
+fn find_device(path: &Path, device_name: &str, visited: &mut HashSet<PathBuf>) -> Option<PathBuf> {
+    if !path.is_dir() {
+        return None;
+    }
+
+    let path = std::fs::canonicalize(path).ok()?;
+    if !visited.insert(path.clone()) {
+        return None;
+    }
+
+    check_directory(&path, device_name).or_else(|| {
+        std::fs::read_dir(path).ok()?.flatten().find_map(|entry| find_device(&entry.path(), device_name, visited))
+    })
+}
+
+
+fn toggle_inhibited(path: &Path) -> Result<(), std::io::Error> {
+    let new_value = match std::fs::read_to_string(path)?.trim() {
+        "0" => "1",
+        "1" => "0",
+        _ => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Invalid inhibited file content")),
+    };
+    std::fs::write(path, new_value)
 }
